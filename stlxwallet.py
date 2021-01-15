d09d284a20863d253c9b1937f90c9a80a1d1edf6
STLX Coin <75624217+stlxonline@users.noreply.github.com>
2021-01-15 07:09:26 +0100

    mainnet changes

--- stlxwallet.py
+++ stlxwallet.py
@@ -18,17 +18,6 @@ import random, string
 
 warnings.filterwarnings("ignore", category=FutureWarning)
 
-class bcolors:
-	HEADER = ''
-	OKBLUE = ''
-	SBLUE = ''
-	OKGREEN = ''
-	WARNING = ''
-	FAIL = ''
-	ENDC = ''
-	BOLD = ''
-	UNDERLINE = ''
-
 hash_functions = {
 	'md5': hashlib.md5,
 	'sha1': hashlib.sha1,
@@ -40,6 +29,7 @@ hash_functions = {
 
 HASH_TYPE = 'sha256'
 sys.tracebacklimit = 0
+node = "node1.stlx.online"
 
 def randomword(length):
 	letters = string.ascii_lowercase
@@ -59,7 +49,7 @@ def ec_sign(data, key):
 	return base58.b58encode(signature)
 
 def check_wallet_build(node):
-	build = 4
+	build = 5
 	try:
 		nresponse = requests.get('https://' + str(node) + '/server.php?q=walletbuild')
 		data = nresponse.json()
@@ -96,31 +86,11 @@ def check_nodes():
 	return node
 
 def hash_func(*args) -> bytes:
-	"""Concatenate all the arguments and hash the result.
-	   Note that the hash function used can be modified
-	   in the global parameter HASH_TYPE.
-	Args:
-		*args: Arguments to concatenate
-	Returns:
-		str: The hashed string
-	"""
 	t = b''.join(str(arg).encode('utf-8') for arg in args)
 	return hash_functions[HASH_TYPE](t).digest()
 
 
 def expand(buf, cnt, space_cost) -> int:
-	"""First step of the algorithm. Fill up a buffer with
-	   pseudorandom bytes derived from the password and salt
-	   by computing repeatedly the hash function on a combination
-	   of the password and the previous hash.
-	Args:
-		buf (list str): A list of hashes as bytes.
-		cnt (int): Used in a security proof (read the paper)
-		space_cost (int): The size of the buffer
-	Returns:
-		void: Updates the buffer and counter, but does not
-		return anything.
-	"""
 	for s in range(1, space_cost):
 		buf.append(hash_func(cnt, buf[s - 1]))
 		cnt += 1
@@ -128,22 +98,6 @@ def expand(buf, cnt, space_cost) -> int:
 
 
 def mix(buf, cnt, delta, salt, space_cost, time_cost):
-	"""Second step of the algorithm. Mix time_cost number
-	   of times the pseudorandom bytes in the buffer. At each
-	   step in the for loop, update the nth block to be
-	   the hash of the n-1th block, the nth block, and delta
-	   other blocks chosen at random from the buffer.
-	Args:
-		buf (list str): A list of hashes as bytes.
-		cnt (int): Used in a security proof (read the paper)
-		delta (int): Number of random blocks to mix with.
-		salt (str): A user defined random value for security
-		space_cost (int): The size of the buffer
-		time_cost (int): Number of rounds to mix
-	Returns:
-		void: Updates the buffer and counter, but does not
-		return anything.
-	"""
 	for t in range(time_cost):
 		for s in range(space_cost):
 			buf[s] = hash_func(cnt, buf[s - 1], buf[s])
@@ -154,83 +108,70 @@ def mix(buf, cnt, delta, salt, space_cost, time_cost):
 				buf[s] = hash_func(cnt, buf[s], buf[other])
 				cnt += 1
 
-
 def extract(buf) -> bytes:
-	"""Final step. Return the last value in the buffer.
-	Args:
-		buf (list str): A list of hashes as bytes.
-	Returns:
-		str: Last value of the buffer as bytes
-	"""
 	return buf[-1]
 
 
 def balloon(password, salt, space_cost, time_cost, delta=3) -> bytes:
-	"""Main function that collects all the substeps. As
-	   previously mentioned, first expand, then mix, and 
-	   finally extract. Note the result is returned as bytes,
-	   for a more friendly function with default values
-	   and returning a hex string see the function balloon_hash
-	Args:
-		password (str): The main string to hash
-		salt (str): A user defined random value for security
-		space_cost (int): The size of the buffer
-		time_cost (int): Number of rounds to mix
-		delta (int): Number of random blocks to mix with.
-	Returns:
-		str: A series of bytes, the hash.
-	"""
 	buf = [hash_func(0, password, salt)]
 	cnt = 1
-
 	cnt = expand(buf, cnt, space_cost)
 	mix(buf, cnt, delta, salt, space_cost, time_cost)
 	return extract(buf)
 
 
 def balloon_hash(password, salt):
-	"""A more friendly client function that just takes
-	   a password and a salt and computes outputs the hash in hex.
-	Args:
-		password (str): The main string to hash
-		salt (str): A user defined random value for security
-	Returns:
-		str: The hash as hex.
-	"""
 	delta = 6
 	time_cost = 12
 	space_cost = 24
 	return balloon(password, salt, space_cost, time_cost, delta=delta).hex()
 
+def get_result(hashv):
+    positions = [1, 2, 3, 5, 7, 11, 13, 17]
+    val = 0;
+    n=1;
+    maxd = 0;
+    for pos in positions:
+        val = val + (int(hashv[pos], 16)*(16**n))
+        n = n + 1
+        maxd = maxd + (15*(16**n))
+
+    diff = int(maxd/val)
+    return int(diff/30)
+
 def worker(num, address, node, dictmgr, diff, miningid, s):
 	dictmgr[1] = 0
 	dictmgr[2] = 0
-	dictmgr[3] = []
+	dmgr3 = [0, "", ""]
+	dmgr3[0] = 0
+	dmgr3[1] = ""
+	dmgr3[2] = ""
+	dictmgr[3] = dmgr3
+	dmgr4 = [0, "", ""]
 	response = ""
 	run = 1
 	errors = 0
+	decimals = 100000000
 	
 	while(run):
 		try:
 			nresponse = s.get('https://' + str(node) + '/server.php?q=getminingtemplate&id=' + str(miningid))
 			data = nresponse.json()
 			dictmgr[1] = data
-			nresponse = s.get('https://stlx.online/server.php?q=getpoolbalance&a=' + str(address))
-			balance = nresponse.json()
-			#print(str(dictmgr[1]))
+			#nresponse = s.get('https://' + str(node) + '/server.php?q=getpoolbalance&a=' + str(address))
+			#balance = nresponse.json()
 			if response != dictmgr[1]:
 				response = dictmgr[1]
 				if num == 0:
-					print("[Mining] New block: " + str(dictmgr[1]['result']['height']) + ", Pending balance: " + str(balance['balance']/10000)) + " STLX" # + ", 16block diff: " + str(dictmgr[1]['result']['difficulty']))
-			time.sleep(10)
+					print("[Worker] New block: " + str(dictmgr[1]['result']['height']) + ", Pending balance: " + str(balance['balance']/decimals) + " STLX")
+			time.sleep(30)
 			errors = 0
 			try:
-				if(len(dictmgr[3]) > 500):
-					hashjson = json.dumps(dictmgr[3])
-					dictmgr[2] = dictmgr[2] + len(dictmgr[3])
-					hashes = []
-					dictmgr[3] = hashes
-					sresponse = s.post('https://stlx.online/srng.php?wid=' + address, json=hashjson)
+				dmgr4 = dictmgr[3]
+				nresponse = s.get('https://' + str(node) + '/server.php?q=submitshare&address=' + str(address) + '&diff=' + str(dmgr4[0]) + '&nonce=' + str(dmgr4[1]) + '&hash=' + str(dmgr4[2]))
+				print('[Worker] Share sent with diff: ' + str(dmgr4[0]) + ', hash: ' + str(dmgr4[2]))
+				dictmgr[3] = dmgr3
+				dictmgr[2] = dictmgr[2] + 1
 			except Exception as e:
 				print(e)
 		except Exception as e:
@@ -243,24 +184,27 @@ def worker(num, address, node, dictmgr, diff, miningid, s):
 
 
 def mining(num, address, privkey, pubkey, miningid, cores, dictmgr, diff):
-	decimal = 10000
 	printed = 0
 	n = 0
 	it = int(time.time())
 	response = ""
 	run = 1
 	errors = 0
+	dmgr3 = [0, "", ""]
 	
 	while(run):
 		try:
 			if (int(time.time()) % 30 == num and int(time.time()) != printed):
 				printed = int(time.time())
-				print("[Mining] " + "Thread " + str(num) + ": " + str(round(n/(int(time.time()+1)-it),2)) + " h/s, Shares: " + str(dictmgr[2]))
+				print("[Hashing] " + "Thread " + str(num) + ": " + str(round(n/(int(time.time()+1)-it),2)) + " h/s, Shares: " + str(dictmgr[2]))
 			a = randomword(16)
 			res = balloon_hash(address + "-" + str(dictmgr[1]['result']['height']) + "-" + str(dictmgr[1]['result']['difficulty']) + "-" + str(dictmgr[1]['result']['prevhash']), a)
-			hashes = dictmgr[3]
-			hashes.append([a, res])
-			dictmgr[3] = hashes
+			dmgr3 = dictmgr[3]
+			if get_result(res) > dmgr3[0]:
+				dmgr3[0] = get_result(res)
+				dmgr3[1] = a
+				dmgr3[2] = res
+				dictmgr[3] = dmgr3
 			n = n+1
 			errors = 0
 		except Exception as e:
@@ -276,6 +220,7 @@ def open_wallet(filename, password):
 	try:
 		f = open(openfile, 'r')
 		a = f.read()
+		privkey = a
 		a = litering_by_64(a)
 		a = "-----BEGIN ENCRYPTED PRIVATE KEY-----\n" + a + "\n-----END ENCRYPTED PRIVATE KEY-----\n"
 		f.close()
@@ -285,17 +230,12 @@ def open_wallet(filename, password):
 		return 0
 	try:
 		k = crypto.load_privatekey(crypto.FILETYPE_PEM, a.encode('utf-8'), password.encode('utf-8'))
-		a = crypto.dump_privatekey(crypto.FILETYPE_PEM, k).decode('utf-8')
 	except Exception as e:
 		print("Error opening wallet. Invalid password? Please, try again.")
 		print("")
 		return 0
 	rawprivatekey = k
-	a = a.replace("-----BEGIN PRIVATE KEY-----", "")
-	a = a.replace("-----END PRIVATE KEY-----", "")
-	a = a.replace("\n", "")
-	ab = base64.b64decode(a)
-	ac = base58.b58encode(ab)
+	privkey = base58.b58encode(privkey) #ac = base58.b58encode(ab)
 	b = crypto.dump_publickey(crypto.FILETYPE_PEM, k).decode('utf-8')
 	b = b.replace("-----BEGIN PUBLIC KEY-----", "")
 	b = b.replace("-----END PUBLIC KEY-----", "")
@@ -309,7 +249,7 @@ def open_wallet(filename, password):
 	d = hashlib.sha512(d).hexdigest()
 	d = base58.b58encode(d)
 	address = "STLX" + d.decode('utf-8')
-	walletinfo = [address, ac, pubkey, rawprivatekey]
+	walletinfo = [address, privkey, pubkey, rawprivatekey]
 	return walletinfo
 
 def print_help():
@@ -335,10 +275,9 @@ def print_help():
 
 def suboption(walletinfo):
 	suboption = ""
-	decimal = 10000
-	maxfee = 1000
+	decimal = 100000000
+	pdecimal = 8
 	minfee = 1
-	node = "stlx.online"
 	vfee = 0.0001
 	server = 1
 	ismining = 0
@@ -368,27 +307,27 @@ def suboption(walletinfo):
 			print("")
 		if soption[0] == "balance":
 			print("")
-			response = requests.get('https://stlx.online/server.php?q=getbalance&address=' + str(walletinfo[0]))
+			response = requests.get('https://' + str(node) + '/server.php?q=getbalance&address=' + str(walletinfo[0]))
 			bdata = response.json()
-			print ("Available: " + str(round(float(bdata['balance'])/decimal, 4)) + " STLX\nLocked: " + str(round(float(bdata['locked'])/decimal, 4)) + " STLX")
+			print ("Available: " + str(round(float(bdata['balance'])/decimal, pdecimal)) + " STLX\nLocked: " + str(round(float(bdata['locked'])/decimal, pdecimal)) + " STLX")
 			print("")
 		if soption[0] == "tokens":
 			print("")
-			response = requests.get('https://stlx.online/server.php?q=gettokensbalance&address=' + str(walletinfo[0]))
+			response = requests.get('https://' + str(node) + '/server.php?q=gettokensbalance&address=' + str(walletinfo[0]))
 			bdata = response.json()
 			for token in bdata['tokensbalance']:
-				tresponse = requests.get('https://stlx.online/server.php?q=gettokeninfo&token=' + str(token))
+				tresponse = requests.get('https://' + str(node) + '/server.php?q=gettokeninfo&token=' + str(token))
 				tdata = tresponse.json()
 				print("---- " + str(tdata["token"]["name"]) + " ----")
-				print("Available: " + str(round(float(bdata['tokensbalance'][token][1])/100000000, 4)) + " " + str(token) + "\nLocked: " + str(round(float(bdata['tokensbalance'][token][0])/100000000, 4)) + " " + str(token))
+				print("Available: " + str(round(float(bdata['tokensbalance'][token][1])/decimal, pdecimal)) + " " + str(token) + "\nLocked: " + str(round(float(bdata['tokensbalance'][token][0])/decimal, pdecimal)) + " " + str(token))
 				print("")
 		if soption[0] == "keys":
 			print("")
 			print("Private key image: ")
-			print(walletinfo[1])
+			print(walletinfo[1].decode("utf-8"))
 			print("")
 			print("Public key image: ")
-			print(walletinfo[2])
+			print(walletinfo[2].decode("utf-8"))
 			print("")
 		if soption[0] == "version":
 			print("")
@@ -402,8 +341,6 @@ def suboption(walletinfo):
 					amount = amount*decimal
 					dest = suboption.split(" ")[1]
 					fee = round(amount*vfee, 0)
-					if fee > maxfee:
-						fee = maxfee
 					if fee < minfee:
 						fee = minfee
 					amount = int(amount)
@@ -414,14 +351,14 @@ def suboption(walletinfo):
 					txinfo = str(amount) + "-" + str(fee) + "-"  + str(dest) + "-" + str(message) + "-" + str(version) + "-" + str(walletinfo[2]) + "-" + str(date)
 					signature = ec_sign(txinfo, walletinfo[3])
 					validateinput = ""
-					print("Sending " + str(round(float(amount)/decimal, 4)) + " STLX to " + dest + ", with fees: " + str(round(float(fee)/decimal, 4)) + " STLX.")
+					print("Sending " + str(round(float(amount)/decimal, pdecimal)) + " STLX to " + dest + ", with fees: " + str(round(float(fee)/decimal, pdecimal)) + " STLX.")
 					print("")
 					while validateinput.lower() != "y" and validateinput.lower() != "n":
 						validateinput = input("Do it? y/n?")
 						if validateinput.lower() == "y":
 							print("")
 							print("Sending...")
-							url = 'https://stlx.online/server.php?q=transfer'
+							url = 'https://' + str(node) + '/server.php?q=transfer'
 							txvalues = {'amount' : str(amount), 'fee' : str(fee), 'dest' : str(dest), 'pubkey' : str(walletinfo[2]), 'date' : str(date), 'version' : str(version), 'message' : str(message), 'signature' : str(signature) }
 							txjson = json.dumps(txvalues)
 							txresponse = s.post(url, json=txjson)
@@ -443,10 +380,11 @@ def suboption(walletinfo):
 			if len(soption) == 4:
 				try:
 					token = str(soption[1])
-					tresponse = requests.get('https://stlx.online/server.php?q=gettokeninfo&token=' + str(token))
+					tresponse = requests.get('https://' + str(node) + '/server.php?q=gettokeninfo&token=' + str(token))
 					tdata = tresponse.json()
 					if tdata["status"] == "OK":
 						tokendecimal = pow(10, int(tdata["token"]["decimals"]))
+						ptokendecimal = int(tdata["token"]["decimals"])
 						amount = float(soption[3])
 						amount = amount*tokendecimal
 						dest = suboption.split(" ")[2]
@@ -459,14 +397,14 @@ def suboption(walletinfo):
 						txinfo = str(amount) + "-" + str(fee) + "-"  + str(dest) + "-" + str(message) + "-" + str(version) + "-" + str(walletinfo[2]) + "-" + str(date) + "-" + str(token)
 						signature = ec_sign(txinfo, walletinfo[3])
 						validateinput = ""
-						print("Sending " + str(round(float(amount)/tokendecimal, 4)) + " " + str(token) + " to " + dest + ", with fees: " + str(round(float(fee)/decimal, 4)) + " STLX.")
+						print("Sending " + str(round(float(amount)/tokendecimal, ptokendecimal)) + " " + str(token) + " to " + dest + ", with fees: " + str(round(float(fee)/decimal, ptokendecimal)) + " STLX.")
 						print("")
 						while validateinput.lower() != "y" and validateinput.lower() != "n":
 							validateinput = input("Do it? y/n?")
 							if validateinput.lower() == "y":
 								print("")
 								print("Sending...")
-								url = 'https://stlx.online/server.php?q=transfer&symbol=' + str(token)
+								url = 'https://' + str(node) + '/server.php?q=transfer&symbol=' + str(token)
 								txvalues = {'amount' : str(amount), 'fee' : str(fee), 'dest' : str(dest), 'pubkey' : str(walletinfo[2]), 'date' : str(date), 'version' : str(version), 'message' : str(message), 'signature' : str(signature) }
 								txjson = json.dumps(txvalues)
 								txresponse = s.post(url, json=txjson)
